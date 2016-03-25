4eb0c8783d0c576ed24f4205b5d3eb7ae5dd9532
NT <nickt444@gmail.com>
2016-03-24 23:18:08 -0400

    added minimap

--- client/app/map.controller.js
+++ client/app/map.controller.js
@@ -5,6 +5,12 @@ app.controller('MapCtrl', function($scope, Auth ){
 
     $scope.user = {};
 
+    var map = L.map('map').setView([34.326, -117.697], 12);
+
+    L.esri.basemapLayer('Topographic').addTo(map);
+
+    var miniMap = new L.Control.MiniMap(L.esri.basemapLayer('Topographic'),{ toggleDisplay: true}).addTo(map);
+
 
 
 })
