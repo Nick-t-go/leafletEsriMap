780717346812a90324f1221d3f80536fcc1661df
Nick Toscano <ntoscano@gdb.gayrondebruin.com>
2016-03-28 15:33:22 -0400

    cleaned up ui

--- client/app/map.controller.js
+++ client/app/map.controller.js
@@ -6,6 +6,24 @@ app.controller('MapCtrl', function($scope, Auth ){
   $scope.displayInfo = false;
   $scope.selectedFeatures = [];
 
+  $scope.returnRelated = function(objectId){
+         $scope.query.objectIds([objectId]).relationshipId(6).run(function(error, response, raw) {
+           $scope.data = response.features;
+           $scope.data[0].style = {'background-color': 'lightgrey'}
+           $scope.contract = response.features[0].properties
+           $scope.$digest()
+         })
+         $("#contracts").tab('show')
+  }
+  $scope.returnContract =  function(selectedItem){
+    $scope.data.forEach(function(item){
+      item.properties === selectedItem ? item.style = {'background-color': 'lightgrey'} : item.style = {}
+    })
+    $scope.contract = selectedItem;
+    $("#contract-details").tab('show')
+
+  }
+
   $scope.activateContracts = function(){
     $scope.displayInfo = !$scope.displayInfo;
   }
@@ -17,11 +35,11 @@ app.controller('MapCtrl', function($scope, Auth ){
     L.esri.basemapLayer('Topographic').addTo(map);
 
     var miniMap = new L.Control.MiniMap(L.esri.basemapLayer('Topographic'),{ toggleDisplay: true}).addTo(map);
-    $scope.data = "Hey There";
+    $scope.data = [];
     function serverAuth(callback){
        L.esri.post('https://fs-gdb10:6443/arcgis/tokens/generateToken', {
          username: 'ntoscano',
-         password: "****",
+         password: "***!",
          f: 'json',
          expiration: 86400,
          client: 'referer',
@@ -45,13 +63,14 @@ app.controller('MapCtrl', function($scope, Auth ){
            e.authenticate(response.token);
          });
        });
-       var query = L.esri.Related.query(dl);
+       $scope.query = L.esri.Related.query(dl);
 
        dl.on('mouseover', highlightFeature);
        dl.on('mouseout', resetHighlight);
+
+
        
        function highlightFeature(e) {
-        console.log(dl._layers[1].feature.properties.ContractNumber)
         var layer = e.layer;
         layer.setStyle({
           weight: 5,
@@ -69,6 +88,19 @@ app.controller('MapCtrl', function($scope, Auth ){
         layer._id = feature.id
       }
 
+      $scope.getLayerHighlight=function(id){
+        var e = {}
+        e.layer = dl._layers[id]
+        highlightFeature(e)
+      }
+
+      $scope.getLayerReset=function(id){
+        console.log("reset")
+        var e = {}
+        e.layer = dl._layers[id]
+        resetHighlight(e)
+      }
+
 
       function resetHighlight(e) {
         var layer = e.layer
@@ -83,24 +115,19 @@ app.controller('MapCtrl', function($scope, Auth ){
        dl.on("click", queryRelated);
 
        function queryRelated (evt) {
+        $("#selected-features").tab('show')
+        $scope.selectedFeatures = []
         // console.log(dl.query().nearby(evt.latlng,1).featureIds(function(ids){return ids}))
         dl.query().nearby(evt.latlng,1).ids(function(error, ids){
-          forEach})
+          if(ids){
+            ids.forEach(function(id){
+              $scope.selectedFeatures.push(dl._layers[id].feature.properties)
+            })
+          }
+          $scope.$digest()
+        })
         //if multiple ids let user click one polygon
         //else run function that returns records
-        if($scope.displayInfo)
-          //show records
-          //then have user select record to display particular record 
-         query.objectIds([evt.layer.feature.id]).relationshipId(6).run(function(error, response, raw) {
-         	console.log(response.features)
-           $scope.data = response.features[0].properties;
-           $scope.$digest()
-         })
        }
-
-
      });
-
-
-
-})
+   })
